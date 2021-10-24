5cf3709136b03ef54121f6955aec4431feb7a798
Andrei Vetrov <vettrov78@gmail.com>
2021-10-24 17:39:17 +0300

    Футер до PP сделан

--- build/js/script.js
+++ build/js/script.js
@@ -31,7 +31,43 @@ callRequestButton.addEventListener('click', function () {
 
 });
 
+//Реализация аккордеона
+var maBlock = document.querySelector('.page-footer__menu-address-wrapper');
+maBlock.classList.remove('no-js');
+var menuBlock = document.querySelector('.page-footer__menu-block');
+menuBlock.classList.add('page-footer__menu-block--closed');
+var addressBlock = document.querySelector('.page-footer__address-block');
+addressBlock.classList.add('page-footer__address-block--closed');
 
+var menuToggle = document.querySelector('.page-footer__menu-toggle');
+menuToggle.addEventListener('click', function () {
+  if (menuBlock.classList.contains('page-footer__menu-block--closed')) {
+    menuBlock.classList.remove('page-footer__menu-block--closed');
+    menuBlock.classList.add('page-footer__menu-block--opened');
+    if (addressBlock.classList.contains('page-footer__address-block--opened')) {
+      addressBlock.classList.add('page-footer__address-block--closed');
+      addressBlock.classList.remove('page-footer__address-block--opened');
+    }
+  } else {
+    menuBlock.classList.add('page-footer__menu-block--closed');
+    menuBlock.classList.remove('page-footer__menu-block--opened');
+  }
+})
+
+var addressToggle = document.querySelector('.page-footer__address-toggle');
+addressToggle.addEventListener('click', function () {
+  if (addressBlock.classList.contains('page-footer__address-block--closed')) {
+    addressBlock.classList.remove('page-footer__address-block--closed');
+    addressBlock.classList.add('page-footer__address-block--opened');
+    if (menuBlock.classList.contains('page-footer__menu-block--opened')) {
+      menuBlock.classList.add('page-footer__menu-block--closed');
+      menuBlock.classList.remove('page-footer__menu-block--opened');
+    }
+  } else {
+    addressBlock.classList.add('page-footer__address-block--closed');
+    addressBlock.classList.remove('page-footer__address-block--opened');
+  }
+})
 // var headerToggle = document.querySelector('.page-header__navigation-toggle');
 
 // pageHeader.classList.remove('page-header--nojs');
