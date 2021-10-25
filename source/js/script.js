7b18e9089935b4cd1dbccaa6d091bb15b43e7cd8
Andrei Vetrov <vettrov78@gmail.com>
2021-10-25 10:37:24 +0300

    Блок преимущества

--- source/js/script.js
+++ source/js/script.js
@@ -68,16 +68,3 @@ addressToggle.addEventListener('click', function () {
     addressBlock.classList.remove('page-footer__address-block--opened');
   }
 })
-// var headerToggle = document.querySelector('.page-header__navigation-toggle');
-
-// pageHeader.classList.remove('page-header--nojs');
-
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header__navigation--closed')) {
-//     pageHeader.classList.remove('page-header__navigation--closed');
-//     pageHeader.classList.add('page-header__navigation--opened');
-//   } else {
-//     pageHeader.classList.add('page-header__navigation--closed');
-//     pageHeader.classList.remove('page-header__navigation--opened');
-//   }
-// });
